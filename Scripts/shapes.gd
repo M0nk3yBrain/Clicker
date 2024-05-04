72296b1b1decdab1ebd5122b7ebc92abcc3ab7fe
Jonathan Melchor <melchorj@zohomail.com>
2024-05-03 23:03:51 -0500

    Timer update

--- Scripts/shapes.gd
+++ Scripts/shapes.gd
@@ -1,14 +1,17 @@
 extends Node2D
 
+@onready var label = $Label
 
-var speed = 0.1
+
+var speed = randf_range(.1, .5)
+var power = 10
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	label.text = "%s" % power
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position += Vector2(0, .5)
+	position += Vector2(0, speed)
