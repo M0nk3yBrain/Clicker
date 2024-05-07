8abe362d7a11b5a6b2d70c5ffb2901ce6ece31ef
Jonathan Melchor <melchorj@zohomail.com>
2024-05-06 20:42:14 -0500

    Started horizontal movment
    On Triangle collision with group "Walls" * -1. Cause side to side movement.

--- Scripts/shapes.gd
+++ Scripts/shapes.gd
@@ -5,6 +5,7 @@ extends Node2D
 
 var speed = randf_range(.1, .5)
 var power = 10
+var side_movement = 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,4 +15,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position += Vector2(0, speed)
+	position += Vector2(side_movement, speed)
+
+
+func _bounce(area):
+	if is_in_group("Walls"):
+		side_movement *= -1
