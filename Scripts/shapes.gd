bf5f7e6bb492297860b0e4153a6fe42aba5569c0
Jonathan Melchor <melchorj@zohomail.com>
2024-06-21 16:41:09 -0500

    adding click

--- Scripts/shapes.gd
+++ Scripts/shapes.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var label = $Label
 
 
+var entered: bool
 var speed = .3
 var power = 10
 @export var side_movement = 0
@@ -11,6 +12,10 @@ var power = 10
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	label.text = "%s" % power
+
+
+func _process(delta):
+	_click()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,3 +31,17 @@ func _bounce(area):
 func _on_area_entered(area):
 	if area.is_in_group("Walls"):
 		side_movement *= -1
+
+
+func _on_mouse_entered():
+	entered = true
+
+
+func _on_mouse_exited():
+	entered = false
+
+
+func _click():
+	if Input.is_action_just_pressed("click"):# and entered == true:
+		print("Click")
+		queue_free()
